08b5999993e5898fa6ab6e93b153c1fabeea928c
zandivx <andreas.zach1@gmail.com>
2021-04-30 20:27:31 +0200

    settings.json for biblatex added

---
+++ Python/Fehlerrechung_Dominik.py
@@ -0,0 +1,22 @@
+from uncertainties import ufloat
+from uncertainties.umath import *
+
+alpha = ufloat(radians(13.5), radians(0.5))
+s = ufloat(0.1485, 0.0005)
+t = ufloat(0.76, 0.12)
+g = 9.81
+
+names = ["alpha", "s", "t", "g"]
+variables = [alpha, s, t]
+
+function = (sin(alpha) - (2*s)/(g * t**2)) / cos(alpha)
+
+derivatives = [function.derivatives[variable] for variable in variables]
+terms = [abs(derivative * variable.s) for variable, derivative in zip(variables, derivatives)]
+
+for name, derivative, term in zip(names, derivatives, terms):
+    print(f"Derivative {name}:")
+    print(f"  Factor:\t{derivative}")
+    print(f"  Term:\t\t{term}\n")
+
+print(f"Total uncertainty is {sum(terms)}")
---
+++ Python/error_propagation.py
@@ -0,0 +1,8 @@
+import uncertainties as u
+import uncertainties.umath as um
+import uncertainties.unumpy as unp
+
+tuple_ = (u, um, unp)
+
+for i in tuple_:
+    print(f"{i}: {dir(i)}\n")
