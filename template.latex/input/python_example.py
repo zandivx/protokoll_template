dd44a92ca8d2390c8e2018aa9a6dd3e0351b579b
zandivx <andreas.zach@gmail.com>
2022-04-14 15:21:02 +0200

    minor changes in preamble, added longtblr customizations

---
+++ template.latex/input/python_example.py
@@ -0,0 +1,11 @@
+"""A simple example for a few types and keywords"""
+
+from math import pi, floor
+
+CONST = 4
+str_ = "abcde"
+
+# calculate output
+output = str_*CONST+str(2)*floor(pi)
+
+print(output)
