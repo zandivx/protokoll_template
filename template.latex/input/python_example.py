45d5aa761f4c18ac1a730acfb7e10bad16293884
zandivx <andreas.zach1@gmail.com>
2022-10-20 00:01:15 +0200

    Changed listings font to FiraCode

--- template.latex/input/python_example.py
+++ template.latex/input/python_example.py
@@ -6,6 +6,9 @@ CONST = 4
 str_ = "abcde"
 
 # calculate output
-output = str_*CONST+str(2)*floor(pi)
+output = str_ * CONST + str(2) * floor(pi)
+
+# Fira Code tests:
+# r 0 @ & * == === != !==
 
 print(output)
